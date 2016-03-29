5ad537dd9d168dbcaeb9b5fff1c6bc8efdb43dfa
EduardoHFreitas <eduardo.freitas@datacoper.com.br>
2016-03-29 20:37:04 -0300

    Controller

---
+++ src/br/univel/view/PainelPrincipalController.java
@@ -0,0 +1,42 @@
+package br.univel.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class PainelPrincipalController {
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private TextField nome;
+
+    @FXML
+    private TextField telefone;
+
+    @FXML
+    private Button excluir;
+
+    @FXML
+    private Button salvar;
+
+    @FXML
+    private Button limpar;
+
+    @FXML
+    void Excluir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Limpar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Salvar(ActionEvent event) {
+
+    }
+
+}
