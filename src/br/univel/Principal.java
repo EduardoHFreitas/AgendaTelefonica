4c6c21681f7f4b35b6815d208f84fe1da1616465
EduardoHFreitas <eduardo.freitas@datacoper.com.br>
2016-04-06 22:04:11 -0300

    commit

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -1,5 +1,26 @@
 package br.univel;
 
-public class Principal {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.fxml.FXMLLoader;
 
+public class Principal extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("PainelPrincipal.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
